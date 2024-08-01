70439ee8c40df55934ac8ec79bd20fef81159669
suryamkotera906609864 <suryamkotera@vt.edu>
2024-08-01 12:19:09 -0400

    first edit

---
+++ src/constants/index.js
@@ -0,0 +1,262 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Software Engineering Fellow",
+      icon: web,
+    },
+    {
+      title: "Undergraduate Researcher",
+      icon: mobile,
+    },
+    {
+      title: "Perception Design Programmer",
+      icon: backend,
+    },
+    {
+      title: "Microelectronics Intern",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Microelectronics Technical Intern",
+      company_name: "NOVA Systemic",
+      icon: backend,
+      iconBg: "#2e1065",
+      date: "Jul. 2022 - Aug. 2022",
+      points: [
+        "Fulfilled HR functions and curriculum review as well as assist in set up of the camp classroom on location.",
+        "Built numerous projects with Raspberry Pi and Arduino including an atmospheric temperature reader using a touch sensor to receive voltage from the air.",
+        " Aided students to build and create their own projects using Arduino and Raspberry Pi for Engineering showcase."
+      ],
+    },
+    {
+      title: "VT AutoDrive Perception Design Programmer",
+      company_name: "Virginia Tech",
+      icon: backend,
+      iconBg: "#2e1065",
+      date: "Sep. 2023 - May 2024",
+      points: [
+        "Worked on Vehicle-to-Anything communication software using Python that enables vehicles to exchange data and communicate within its environment",
+        "Implementing an Object Analysis node in the vehicle using ROS and C++ to detect road obstacles and hazards to the vehicle.",
+        " Will compete and fully test drive the autonomous vehicle in the AutoDrive Collegiate Competition at University of Michigan."
+        
+      ],
+    },
+    {
+      title: "Undergraduate Researcher",
+      company_name: "Virginia Tech IDPro",
+      icon: backend,
+      iconBg: "#2e1065",
+      date: "Dec. 2023 - Present",
+      points: [
+        " Utilizing computer vision and machine learning algorithms, such as YOLO and UNet to provide precise and efficient measurements of various growth parameters in plants advised by Dr. Zhao.",
+        " Working on quantifying growth parameters such as plant height, leaf area, and stem diameter, to gain a deeper understanding of plant physiology and overall health.",
+      ],
+    },
+    {
+      title: "Software Engineering Fellow",
+      company_name: "Headstarter AI",
+      icon: backend,
+      iconBg: "#2e1065",
+      date: "July 2024 - Present",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, projects };
